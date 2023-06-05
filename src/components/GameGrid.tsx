d44073b4b73078f22f4469e0e04968d906dd491b
Renee <rmmason@gmail.com>
2023-06-04 18:07:35 -0700

    Building game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,38 +1,19 @@
-import React, { useState, useEffect } from "react"
-import apiClient from "../services/api-client";
-import { Text } from '@chakra-ui/react'
-
-interface Game {
-    id: number;
-    name: string;
-}
-
-interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-}
+import { SimpleGrid, Text } from '@chakra-ui/react'
+import useGames from '../hooks/useGames';
+import GameCard from './GameCard';
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
 
-  useEffect(() => {
-    const controller = new AbortController();
-
-    apiClient
-    .get<FetchGamesResponse>('/games', { signal: controller.signal})
-    .then(res => {
-        setGames(res.data.results)
-    })
-    .catch(err => setError(err.message));
-   }, []);
-
+  const {games, error} = useGames();
+ 
   return (
     <>
-   { error && <Text>{error}</Text>}
-   <ul>
-    {games.map(game => <li key={game.id}>{game.name}</li>)}
-   </ul>
+   {error && <Text>{error}</Text>}
+   <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 , xl: 5}} spacing={10}>
+    {games.map((game) => (
+    <GameCard key={game.id} game={game}></GameCard>
+    ))}
+   </SimpleGrid>
    </>
   )
 }
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+export interface Game {
+    id: number;
+    name: string;
+    background_image: string;
+}
+
+interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState('');
+  
+    useEffect(() => {
+      const controller = new AbortController();
+  
+      apiClient
+      .get<FetchGamesResponse>('/games', { signal: controller.signal})
+      .then(res => {
+          setGames(res.data.results)
+      })
+      .catch(err => setError(err.message));
+     }, []); //array of dependencies
+
+     return {games, error};
+  
+}
+
+export default useGames;
