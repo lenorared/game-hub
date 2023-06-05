950fb8176ff2f3d465f267622afb9e847217e43e
Renee <rmmason@gmail.com>
2023-06-04 21:01:08 -0700

    Show loading skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,12 @@
+import { Card, Skeleton, SkeletonText } from '@chakra-ui/react'
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width='300px' borderRadius={10} overflow='hidden' >
+        <Skeleton height="200px" />
+        <SkeletonText />
+    </Card>
+  )
+}
+
+export default GameCardSkeleton
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,19 +23,25 @@ interface FetchGamesResponse {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState('');
+    const [isLoading, setLoading] = useState(false)
   
     useEffect(() => {
       const controller = new AbortController();
   
+      setLoading(true)
       apiClient
       .get<FetchGamesResponse>('/games', { signal: controller.signal})
       .then(res => {
           setGames(res.data.results)
+          setLoading(false)
       })
-      .catch(err => setError(err.message));
+      .catch(err => {
+        setError(err.message)
+        setLoading(false)
+     });
      }, []); //array of dependencies
 
-     return {games, error};
+     return {games, error, isLoading};
   
 }
 
