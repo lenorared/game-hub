6fe0d113a8363a40ee15afbd97425205e47d3eb0
Renee <rmmason@gmail.com>
2023-06-04 22:06:40 -0700

    Show spin while fetching the genres

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import { Card, Skeleton, SkeletonText } from '@chakra-ui/react'
 const GameCardSkeleton = () => {
   return (
     <Card>
-        <Skeleton height="200px" />
+        <Skeleton />
         <SkeletonText />
     </Card>
   )
