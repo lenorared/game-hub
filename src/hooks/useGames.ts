2528d9c8bc8077ae65e127903a293015670801a2
Renee <rmmason@gmail.com>
2023-06-07 18:17:50 -0700

    add Emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,6 +14,8 @@ export interface Game {
     background_image: string;
     parent_platforms: {platform: Platform } [];
     metacritic: string;
+    rating_top: number;
+    rating: number;
 }
 
 const useGames = (
